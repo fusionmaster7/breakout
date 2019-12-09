a85486083d18f6e4d2e4f7f92ba9f281012f8aad
Hardik Singh <hardiksingh297@gmail.com>
2019-12-09 19:33:47 +0530

    The paddle is now fully functional.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var ball_radius = 10;
 var paddle_height = 10;
 var paddle_width = 75;
 var paddlex = (canvas.width-paddle_width)/2;
+var right_pressed = false;
+var left_pressed = false;
 function draw_ball(){
   ctx.beginPath();
   ctx.arc(x,y,ball_radius,0,Math.PI*2);
@@ -18,7 +20,7 @@ function draw_ball(){
 function draw_paddle(){
   ctx.beginPath();
   ctx.rect(paddlex,canvas.height-paddle_height,paddle_width,paddle_height);
-  ctx.fillStyle = 'red';
+  ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
 }
@@ -33,6 +35,35 @@ function draw(){
   }
   if(x+dx<ball_radius||x+dx>canvas.width-ball_radius){
     dx = -dx;
+  }
+  if(right_pressed){
+    paddlex+=7;
+    if(paddlex+paddle_width>canvas.width){
+      paddlex = canvas.width - paddle_width;
+    }
+  }else if(left_pressed){
+    paddlex-=7;
+    if(paddlex<0){
+      paddlex = 0;
+    }
+  }
+}
+document.addEventListener("keydown",keydown_handler,false);
+document.addEventListener("keyup",keyup_handler,false);
+function keydown_handler(e){
+  if(e.key=="Right"||e.key=="ArrowRight"){
+    right_pressed = true;
+  }
+  if(e.key=="Left"||e.key=="ArrowLeft"){
+    left_pressed = true;
+  }
+}
+function keyup_handler(e){
+  if(e.key=="Right"||e.key=="ArrowRight"){
+    right_pressed = false;
+  }
+  if(e.key=="Left"||e.key=="ArrowLeft"){
+    left_pressed = false;
   }
 }
 setInterval(draw,10);
