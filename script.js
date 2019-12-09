d0352966d03eb976e37cd77fdf00cc4da2675444
Hardik Singh <hardiksingh297@gmail.com>
2019-12-09 19:01:19 +0530

    Created the paddle.

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ball_radius = 10;
+var paddle_height = 10;
+var paddle_width = 75;
+var paddlex = (canvas.width-paddle_width)/2;
 function draw_ball(){
   ctx.beginPath();
   ctx.arc(x,y,ball_radius,0,Math.PI*2);
@@ -12,9 +15,17 @@ function draw_ball(){
   ctx.fill();
   ctx.closePath();
 }
+function draw_paddle(){
+  ctx.beginPath();
+  ctx.rect(paddlex,canvas.height-paddle_height,paddle_width,paddle_height);
+  ctx.fillStyle = 'red';
+  ctx.fill();
+  ctx.closePath();
+}
 function draw(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
   draw_ball();
+  draw_paddle();
   x+=dx;
   y+=dy;
   if(y+dy<ball_radius||y+dy>canvas.height-ball_radius){
